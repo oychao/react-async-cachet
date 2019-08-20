e5b1e8d1ca7804e81ca3ea86b1f06fcdc3118acf
ouyang-chao <chao@ouyang.io>
2019-08-21 02:14:34 +0800

    feature: first version

--- demo/src/components/MsgDisplayer/view.jsx
+++ demo/src/components/MsgDisplayer/view.jsx
@@ -1,9 +1,10 @@
 /* eslint-disable max-len, no-console, class-methods-use-this, react/button-has-type, jsx-a11y/no-redundant-roles, import/no-extraneous-dependencies */
 import React from 'react';
 import { hot } from 'react-hot-loader';
-import { cachet } from 'react-async-cachet';
+import { cachet as methodCachet } from 'react-async-cachet';
 
 const someAsyncAction = () => new Promise(res => setTimeout(res, 3e3, 'Aha!'));
+const someAsyncAction2 = () => new Promise((res, rej) => setTimeout(rej, 3e3, 'Oops, It\'s an error'));
 
 class MsgDisplayer extends React.Component {
   constructor(props) {
@@ -13,7 +14,7 @@ class MsgDisplayer extends React.Component {
     };
   }
 
-  @cachet
+  @methodCachet
   async handleClick() {
     this.setState({
       msg: await someAsyncAction()
@@ -30,7 +31,5 @@ class MsgDisplayer extends React.Component {
     );
   }
 }
-
-console.log(MsgDisplayer.prototype.handleClick);
 
 export default hot(module)(MsgDisplayer);
--- src/main.js
+++ src/main.js
@@ -17,26 +17,12 @@ function puissantAssign(target, source) {
 }
 
 const FLAG = Symbol('REACT_ASYNC_CACHET_FLAG');
+const ERR = Symbol('REACT_ASYNC_CACHET_ERR');
+
+const RawPromise = global.Promise;
+const instMap = new WeakMap();
 
 export function cachet(prototype, prop, descriptor) {
-  // hijack raw promise
-  let inst;
-  const RawPromise = global.Promise;
-  const HijackedPromise = (...args) => {
-    const firstArg = args[0];
-    args[0] = (res, rej) => {
-      firstArg((...args2) => {
-        if (inst && HijackedPromise === global.Promise) {
-          rej('hijack');
-        }
-        res(...args2);
-      }, rej);
-    };
-    return new RawPromise(...args);
-  };
-  puissantAssign(HijackedPromise, RawPromise);
-
-
   // to ensure life methods will be wrapped only once
   if (!prototype[FLAG]) {
     // set flag
@@ -44,20 +30,45 @@ export function cachet(prototype, prop, descriptor) {
 
     // wrap componentWillUnmount
     const wrappedComponentWillUnmount = prototype.componentWillUnmount;
-    const componentWillUnmountDesc = Object.getOwnPropertyDescriptor(prototype, 'wrappedComponentWillUnmount');
+    const componentWillUnmountDesc = Object.getOwnPropertyDescriptor(prototype, 'componentWillUnmount');
     latentSet(prototype, 'componentWillUnmount', function (...args) {
-      inst = this;
-      wrappedComponentWillUnmount.call(this, ...args);
+      // mark current component instance as unmounted
+      instMap.set(this, true);
+      if (wrappedComponentWillUnmount) {
+        wrappedComponentWillUnmount.call(this, ...args);
+      }
     }, componentWillUnmountDesc);
   }
 
   // wrap target method
   const wrappedTargetMethod = prototype[prop];
-  latentSet(prototype, prop, async function (...args) {
+  descriptor.value = async function (...args) {
+    // hijack raw promise
+    const HijackedPromise = (...promiseArgs) => {
+      const firstArg = promiseArgs[0];
+      promiseArgs[0] = (res, rej) => {
+        firstArg((...promiseConstuctorArgs) => {
+          if (instMap.has(this) && HijackedPromise === global.Promise) {
+            rej(ERR);
+          }
+          res(...promiseConstuctorArgs);
+        }, rej);
+      };
+      return new RawPromise(...promiseArgs);
+    };
+    puissantAssign(HijackedPromise, RawPromise);
+
     global.Promise = HijackedPromise;
-    await wrappedTargetMethod.call(this, ...args);
+    try {
+      await wrappedTargetMethod.call(this, ...args);
+    } catch (err) {
+      if (ERR !== err) {
+        throw err;
+      }
+    }
     global.Promise = RawPromise;
-  }, descriptor);
+  };
+  return descriptor;
 }
 
 export default cachet;
