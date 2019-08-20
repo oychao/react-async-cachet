2dd6029f4fba55170b0b8847ce13bb7cd2995f73
ouyang-chao <chao@ouyang.io>
2019-08-21 02:15:14 +0800

    docs: update docs

--- demo/src/components/MsgDisplayer/view.jsx
+++ demo/src/components/MsgDisplayer/view.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable max-len, no-console, class-methods-use-this, react/button-has-type, jsx-a11y/no-redundant-roles, import/no-extraneous-dependencies */
 import React from 'react';
 import { hot } from 'react-hot-loader';
-import { cachet as methodCachet } from 'react-async-cachet';
+import { cachet } from 'react-async-cachet';
 
 const someAsyncAction = () => new Promise(res => setTimeout(res, 3e3, 'Aha!'));
 const someAsyncAction2 = () => new Promise((res, rej) => setTimeout(rej, 3e3, 'Oops, It\'s an error'));
@@ -14,7 +14,7 @@ class MsgDisplayer extends React.Component {
     };
   }
 
-  @methodCachet
+  @cachet
   async handleClick() {
     this.setState({
       msg: await someAsyncAction()
